f1821e87d789369b59a66759e906356c82a5bc7f
b1ueb1ues <bblues@live.cn>
2019-09-29 18:28:30 +0800

    update

--- mod/energy.py
+++ mod/energy.py
@@ -1,33 +1,31 @@
-from adv import *
+from core.ctx import *
 
 
 class Energy():
-    def e_dmg_proc(this, name, amount):
-        this.dmg_proc_old(name, amount)  # should prevent damage in next dmg_proc
+  #  def e_dmg_proc(this, name, amount):
+  #      this.dmg_proc_old(name, amount)  # should prevent damage in next dmg_proc
 
-        if not this.energized :
-            return
-        if this.energized == 1 and name[0] != 's':
-            return
+  #      if not this.energized :
+  #          return
+  #      if this.energized == 1 and name[0] != 's':
+  #          return
 
-        if this.energized == 1:
-            if this.energy_consume and this.energy_consume[name[:2]]:
-                this.energized = name[:2]
-            else:
-                this.energized = name[:2]
+  #      if this.energized == 1:
+  #          if this.energy_consume and this.energy_consume[name[:2]]:
+  #              this.energized = name[:2]
+  #          else:
+  #              this.energized = name[:2]
 
-        if this.energized == name[:2] :
-            boost = this.get_energy_boost()
-            log('dmg','o_%s_energized'%name,amount*boost, 'energy boost')
+  #      if this.energized == name[:2] :
+  #          boost = this.get_energy_boost()
+  #          log('dmg','o_%s_energized'%name, amount*boost, 'energy boost')
 
-    def get_energy_boost(this):
-            sd = this.a.mod('s')
-            this.energy_mod.on()
-            sd2 = this.a.mod('s')
-            this.energy_mod.off()
-            return sd2/sd-1
-
-
+  #  def get_energy_boost(this):
+  #      sd = this.a.Dp.get('s')
+  #      this.energy_mod.on()
+  #      sd2 = this.a.Dp.get('s')
+  #      this.energy_mod.off()
+  #      return sd2/sd-1
 
     def __call__(this):
         if this.energized :
@@ -43,7 +41,8 @@ class Energy():
             self = 1
         elif name == 'team':
             team = 1
-            log('energy','team',team)
+            if log_e:
+                log_e('team',team)
 
         if name in this.energy_self:
             self = this.energy_self[name]
@@ -51,46 +50,45 @@ class Energy():
         if name in this.energy_team:
             team = this.energy_team[name]
             if team:
-                log('energy','team',team)
+                if log_e:
+                    log_e('team',team)
 
         if not this.energized :
             this.energy += self
             if this.energy > 0:
-                this.energy_buff.set(this.energy).on()
+                this.energy_buff.set(this.energy).on(-1)
             if this.energy >= 5 :
                 this.energy = 0
                 this.energized = 1
+                this.energy_mod()
                 this.energized_event()
 
-
-
-    def e_s1_proc(this, e):
+    def e_s1_end(this, e):
         this.add_energy('s1')
-        this.s1_proc_old(e)
+        this.s1_end_old(e)
         if this.energized and this.energized != 1 :
             this.energized = 0
+            this.energy_mod.off()
             this.energy_buff.off()
 
-
-    def e_s2_proc(this, e):
+    def e_s2_end(this, e):
         this.add_energy('s2')
-        this.s2_proc_old(e)
+        this.s2_end_old(e)
         if this.energized and this.energized != 1 :
             this.energized = 0
+            this.energy_mod.off()
             this.energy_buff.off()
 
-
-    def e_s3_proc(this, e):
+    def e_s3_end(this, e):
         this.add_energy('s3')
-        this.s3_proc_old(e)
+        this.s3_end_old(e)
         if this.energized and this.energized != 1 :
             this.energized = 0
+            this.energy_mod.off()
             this.energy_buff.off()
 
     def l_add_energy(this, e):
         this.add_energy(e.name)
-
-
 
     def __init__(this, a, self, team, consume=None):
         this.a = a
@@ -98,25 +96,30 @@ class Energy():
         this.energy_team = team
         this.energy_consume = consume
 
-        this.energy_mod = Modifier('energy_mod','s','passive',0.5).off()
+        this.energy_mod = Passive('energized', 0.5, 's')
 
         this.energy = 0
         this.energized = 0
         this.energized_event = Event('energized')
 
-        this.energy_buff = Buff('energy',-1,-1,'energy','energy')
+        this.energy_buff = Buff('energy',-1,'energy','energy')
 
-        this.s1_proc_old = a.s1_proc
-        this.s2_proc_old = a.s2_proc
-        this.s3_proc_old = a.s3_proc
+        this.s1_end_old = a.s1.conf['on_end']
+        this.s2_end_old = a.s2.conf['on_end']
+        this.s3_end_old = a.s3.conf['on_end']
+        a.s1.conf['on_end'] = this.e_s1_end
+        a.s2.conf['on_end'] = this.e_s2_end
+        a.s3.conf['on_end'] = this.e_s3_end
+        Conf.sync(a.s1.conf)
+        Conf.sync(a.s2.conf)
+        Conf.sync(a.s3.conf)
+
         this.dmg_proc_old = a.dmg_proc
-
         #a.add_energy = this.add_energy
-        a.s1_proc = this.e_s1_proc
-        a.s2_proc = this.e_s2_proc
-        a.s3_proc = this.e_s3_proc
         a.dmg_proc = this.e_dmg_proc
 
-        Listener('add_energy', this.l_add_energy)
+        Listener('add_energy')(this.l_add_energy)
+
+        this.log_e = Logger('energy')
 
 
