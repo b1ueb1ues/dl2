199535f6890b0436f36688a91e9c9642964a03f1
b1ueb1ues <bblues@live.cn>
2019-10-11 15:46:15 +0800

    add fs_alt

---
+++ mod/fs_alt.py
@@ -0,0 +1,41 @@
+import __init__
+from core.ctx import *
+from core.skill import Fs_group
+
+
+class Fs_alt(object):
+    def __init__(this, host, conf2, ssbuff):
+        this.host = host
+        this.level = 0
+
+        conf22 = Conf(conf2)
+
+        this.fs_alt = Fs_group(host, conf22.get)
+        this.log = Logger('fs')
+
+        this.buff = ssbuff
+        ssbuff.on_end.append(this.on_buff_end)
+
+    def off(this):
+        this.buff.off()
+
+    def on_buff_end(this):
+        if this.level == 1:
+            this.host.fs = this.host.a_fs[0]
+            this.level = 0
+            if this.log:
+                this.log('%s, fs_alt -'%(this.host.name))
+
+    def alt(this, duration):
+        this.buff.on(duration)
+        if this.level == 0:
+            this.host.fs = this.fs_alt
+            this.level = 1
+            if this.log:
+                this.log('%s, fs_alt +'%this.host.name)
+    __call__ = alt
+
+    def reset(this):
+        this.host.fs = this.host.a_fs[0]
+        this.level = 0
+
